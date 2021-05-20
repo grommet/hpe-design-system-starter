8621456d81e0de6210f5d385dfba982fff396e66
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-05-19 19:25:25 -0600

    change app.js to use grommet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Grommet } from 'grommet';
+import { hpe } from 'grommet-theme-hpe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={hpe} style={{ height: '100%', width: 'auto' }} full>
+        Hello, World!
+    </Grommet>
   );
 }
 
